3fc3c9d2001745eb31beec14a2fd3d5a2147e775
alishooshtari91 <a.shooshtari91@gmail.com>
2025-12-26 15:17:31 +0330

    created render html files
    added templates folder and three html files named about, index, contact

--- website/views.py
+++ website/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index_view(request):
-    return HttpResponse("home_page")
+    return render(request,template_name="website/index.html")
 def about_view(request):
-    return HttpResponse("about_page")
+    return render(request,template_name="website/about.html")
 def contact_view(request):
-    return HttpResponse("contact_page")
+    return render(request,template_name="website/contact.html")
